d502e5a276b3243424b5f2dcc69b593131330098
eddiechen1 <113886372+eddiechen1@users.noreply.github.com>
2022-09-23 13:11:23 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,3 @@
-var vg_1 = "vizualisation.vg.json";
-vegaEmbed("#vizualisation", vg_1).then(function(result) {
+var vg_1 = "visualization.vg.json";
+vegaEmbed("#visualization", vg_1).then(function(result) {
 }).catch(console.error);
